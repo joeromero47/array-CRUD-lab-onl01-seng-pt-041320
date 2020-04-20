0cc9a6ed35eb8ce8f7c25457ce276d0961d53b34
Joseph Romero <joeromero47@gmail.com>
2020-04-20 02:16:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,6 @@ def add_element_to_end_of_array("array", "element")
   add_element_to_end_of_array << "hi"
   puts add_element_to_end_of_array[2]
 end
-end
 
 
 def add_element_to_start_of_array(array, element)
