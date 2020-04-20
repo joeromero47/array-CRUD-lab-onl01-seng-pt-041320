fe26f3d0ac89b7a0b14ad87a5a912ceb0d1364bd
Joseph Romero <joeromero47@gmail.com>
2020-04-20 02:09:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array << "phrase"
 end
+
 
 def add_element_to_start_of_array(array, element)
   
